2ae54d76d22dc1b02aca5ed01b9269e8fccfc660
Kevin Carpenter <carpenter.kevin@gmail.com>
2022-07-14 00:33:47 -0700

    share image

--- pages/_document.js
+++ pages/_document.js
@@ -17,10 +17,17 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+          <meta property="og:title" content="Adashe" />
+          <meta property="og:site_name" content="Adashe" />
+          <meta property="og:url" content="https://mint.adashe.io" />
+          <meta property="og:description" content="Adashe is cheaper, faster and more efficient than traditional DeFi platforms, making it more accessible to everyone. Learn more here." />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="https://mint.adashe.io/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Hind:wght@400;600&family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
         </Head>
+        <DefaultSeo {...config} />
         <body>
           <Main />
           <NextScript />
